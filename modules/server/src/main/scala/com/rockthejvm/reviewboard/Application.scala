43769841372bd17d9648e351c5cc062ebf51ae1a
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-01-02 17:02:44 +0400

    feat: serve simple health endpoint

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/Application.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/Application.scala
@@ -1,9 +1,37 @@
 package com.rockthejvm.reviewboard
 
 import zio.*
+import sttp.tapir.*
+import zio.http.Server
+import sttp.tapir.server.ziohttp.*
+
 
 object Application extends ZIOAppDefault {
 
-  override def run = ???
+  object EndPoints:
+
+    val health = endpoint
+            // this is an case of a 'builder' pattern
+            .tag("health")
+            .name("health")
+            .description("Health Check")
+            // ^^ this is for documentation (swager etc...)
+            .get // the http method
+            .in("health") // the path
+            .out(plainBody[String]) // simple strings... nothing to parse or serialize
+
+            .serverLogicSuccess[Task](_ /* the request */ => ZIO.succeed("All good"))
+
+  val serverProgram = Server.serve(
+    ZioHttpInterpreter(
+      ZioHttpServerOptions.default // can add configs e.g. CORS for security etc...
+      ).toHttp(
+        EndPoints.health // the route that this server is going to serve
+      )
+    )
+
+  override def run = serverProgram.provide(
+    Server.default
+    )
 
 }
