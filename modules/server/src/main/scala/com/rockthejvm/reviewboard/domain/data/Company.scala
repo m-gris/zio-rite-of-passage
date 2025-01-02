c6e76c889789a176f703d2fb35cd2144112a483e
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-01-02 17:35:12 +0400

    feat: Modeling Company

---
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/domain/data/Company.scala
@@ -0,0 +1,34 @@
+package com.rockthejvm.reviewboard.data
+
+import zio.json.JsonCodec
+import zio.json.DeriveJsonCodec
+
+final case class Company(
+
+  // NON-NULLABLE FIELDS --- start
+  id: Long,
+  name: String,
+  slug: String,
+  url: String,
+  // NON-NULLABLE FIELDS --- end
+
+
+  // OPTIONAL FIELDS
+  location: Option[String] = None,
+  country: Option[String] = None,
+  industry: Option[String] = None,
+  image: Option[String] = None, // BASE 64 -- easy to render on the frontend
+  tags: List[String] = List()
+
+)
+
+
+object Company {
+  /* Exposing the COMPANY as an HTTP PAYLOAD
+   * i.e make the Company instance to be JSON serializable */
+  given codec: JsonCodec[Company] = DeriveJsonCodec.gen[Company] // to serialize BACK & FORTH, TO & FROM JSON
+  // this given instance will be passed automatically whenever we need to
+  //  - return a Company as an HTTP Payload
+  //  - or take a Company as input (as an HTTP Payload)
+}
+
