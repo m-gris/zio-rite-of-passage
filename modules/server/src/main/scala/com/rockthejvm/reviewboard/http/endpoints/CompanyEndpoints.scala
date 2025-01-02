6b365110afe543621b8a2a758914d0b8c7e39aa8
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-01-02 18:02:37 +0400

    feat: endpoints for Company

---
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/http/endpoints/CompanyEndpoints.scala
@@ -0,0 +1,47 @@
+package com.rockthejvm.reviewboard.http.endpoints
+
+// IMPORTS - 3rd parties
+import sttp.tapir.*
+import sttp.tapir.json.zio.*
+import sttp.tapir.generic.auto.* // imports the type class derivation functionallity (to create the JsonCodecs)
+
+// IMPORTS - Local
+import com.rockthejvm.reviewboard.domain.data.*
+import com.rockthejvm.reviewboard.http.requests
+
+
+// NOTA BENE
+// Here we are merely DESCRIBING the endpoints
+// those will be implemented in / as so-called CONTROLLERS
+
+trait CompanyEndpoints {
+
+  val create = endpoint
+    .tag("companies")
+    .name("create")
+    .description("Create a listing for a company")
+    .in("companies")
+    .post
+    .in(jsonBody[requests.CreateCompany])
+    .out(jsonBody[Company])
+
+  val getAll =
+    endpoint
+      .tag("companies")
+      .name("getAll")
+      .description("Get all company listings")
+      .in("companies")
+      .get
+      .out(jsonBody[List[Company]])
+
+  val getById =
+    endpoint
+      .tag("companies")
+      .name("getById")
+      .description("Get a company listing by its ID")
+      .in("companies" / path[String]("id"))
+      .get
+      .out(jsonBody[Option[Company]])
+
+
+}
