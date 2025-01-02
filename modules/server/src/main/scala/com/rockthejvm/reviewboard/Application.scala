7ea80794c92699bf20ea145d4c2c857aed2e13cb
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-01-02 19:11:28 +0400

    feat: create HttAPI to decouple all the endpoints from the Application

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/Application.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/Application.scala
@@ -6,19 +6,19 @@ import zio.http.Server
 import sttp.tapir.server.ziohttp.*
 
 import com.rockthejvm.reviewboard.http.controllers.*
+import com.rockthejvm.reviewboard.http.HttpAPI
 
 
 object Application extends ZIOAppDefault {
 
   val serverProgram = for {
-    controller <- HealthController.makeZIO
+    endpoints  <- HttpAPI.endpointsZIO
     _          <- Server.serve(
                     ZioHttpInterpreter(
                       ZioHttpServerOptions.default // can add configs e.g. CORS for security etc...
-                      ).toHttp(
-                        controller.health // the route that this server is going to serve
-                      )
+                      ).toHttp(endpoints) // the route that this server is going to serve
                     )
+    _          <- Console.printLine("\n\n@@@@ Success - Server Started @@@\n\n")
   } yield ()
 
   override def run = serverProgram.provide(
---
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/http/HttpAPI.scala
@@ -0,0 +1,21 @@
+package com.rockthejvm.reviewboard.http
+
+import sttp.tapir.*
+import com.rockthejvm.reviewboard.http.controllers.*
+
+object HttpAPI {
+  // PURPOSE
+  // to make the server program AGNOSTIC with regard to the different controllers
+  // by COMBINING all the endpoints into a SINGLE COMPREHENSIVE LIST
+
+  def gatherRoutes(controllers: List[BaseController]) =
+    controllers.flatMap(_.routes)
+
+  def makeControllers = for {
+    health <- HealthController.makeZIO
+    companies <- CompanyController.makeZIO
+    // TO DO - add more controllers
+    } yield  List(health, companies)
+
+  val endpointsZIO = makeControllers.map(gatherRoutes)
+}
