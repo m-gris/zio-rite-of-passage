f1d47f7792f36567240ee28c33dec8260fd1d75b
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-01-02 18:32:36 +0400

    feat: method to convert the CreateCompany payload into a instance of our Company class

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/http/requests/CreateCompany.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/http/requests/CreateCompany.scala
@@ -3,6 +3,7 @@ package com.rockthejvm.reviewboard.http.requests
 import zio.json.JsonCodec
 import zio.json.DeriveJsonCodec
 import sttp.tapir.generic.auto._
+import com.rockthejvm.reviewboard.domain.data.Company
 
 final case class CreateCompany(
 
@@ -18,7 +19,20 @@ final case class CreateCompany(
   image: Option[String] = None, // BASE 64 -- easy to render on the frontend
   tags: List[String] = List()
 
-)
+) {
+  def toCompany(id: Long): Company =
+    Company(
+      id=id,
+      slug=Company.makeSlug(name),
+      name=name,
+      url=url,
+      location=location,
+      country=country,
+      industry=industry,
+      image=image,
+      tags=tags
+    )
+}
 
 object CreateCompany {
   given codec: JsonCodec[CreateCompany] = DeriveJsonCodec.gen[CreateCompany]
