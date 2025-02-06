b3bbe91f46825178613e7737cc6b7b97651e7f27
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-02-06 16:38:26 +0545

    feat: errorRoute

--- modules/server/src/main/scala/com/rockthejvm/reviewboard/http/controllers/Health.scala
+++ modules/server/src/main/scala/com/rockthejvm/reviewboard/http/controllers/Health.scala
@@ -1,14 +1,33 @@
 package com.rockthejvm.reviewboard.http.controllers
 
 import zio.*
+import sttp.tapir.*
 import sttp.tapir.server.ServerEndpoint
 
+import com.rockthejvm.reviewboard.domain.errors.*
 import com.rockthejvm.reviewboard.http.endpoints.HealthEndpoint
 
 private class HealthController extends BaseController with HealthEndpoint {
-  val health = check
+
+  val healthRoute = healthEndpoint
+    // serverLogicSucces => guarantees that the endpoint returns a successfull effect.
     .serverLogicSuccess[Task](_ => ZIO.succeed("All good"))
-  override val routes = List(health)
+
+
+  val errorRoute = errorEndpoint
+
+    // serverLogic => Can fail (as opposed to serverLogicSuccess)
+    .serverLogic[Task](
+      _ => ZIO.fail(new RuntimeException("Boom!!!")
+                /* move the Throwable
+                 * from the error channel
+                 * to the value channel
+                 * (as the Left side of an Either)
+                 */
+                ).either) // Task[Either[Throwable, String]]
+
+  override val routes = List(healthRoute, errorRoute)
+
 }
 
 object HealthController {
