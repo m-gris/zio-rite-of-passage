32b0344af62431ab41483abd8b24342b396a65f8
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-04-02 06:51:49 +0800

    feat: basic navLinks rendering (Companies, Login ...)

--- modules/app/src/main/scala/com/rockthejvm/reviewboard/components/Header.scala
+++ modules/app/src/main/scala/com/rockthejvm/reviewboard/components/Header.scala
@@ -6,7 +6,7 @@ import com.raquo.laminar.codecs.*
 import com.raquo.laminar.api.L.{*, given}
 
 object Header {
-  def apply() =
+  def apply() = // boiler-platty stuff
     div(
       cls := "container-fluid p-0",
       div(
@@ -31,9 +31,9 @@ object Header {
             div(
               cls    := "collapse navbar-collapse",
               idAttr := "navbarNav",
-              ul(
-                cls := "navbar-nav ms-auto menu align-center expanded text-center SMN_effect-3"
-                // TODO children
+              ul( // un-ordered list
+                cls := "navbar-nav ms-auto menu align-center expanded text-center SMN_effect-3",
+                render(navLinks)
               )
             )
           )
@@ -41,4 +41,30 @@ object Header {
       )
     )
   )
+
+  val navLinks = List(
+    NavLink("Companies", "/companies"),
+    NavLink("Log In", "/login"),
+    NavLink("Sign Up", "/signup"),
+    )
+
+  private def render(navLink: NavLink) = li(
+    cls := "nav-item",
+    Anchors.renderNavLink(navLink.label, navLink.location, "nav-link jvm-item")
+    )
+
+  private def render(navLinks: List[NavLink]): List[HtmlElement] = navLinks.map(render)
+
+}
+
+case class NavLink(label: String, location: String)
+
+object Anchors {
+
+  def renderNavLink(label: String, location: String, cssClass: String = "") =
+    a(
+      href := location,
+      cls  := cssClass,
+      label
+      )
 }
