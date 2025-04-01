5dd31e6fd561cc49194d346f64f6730d43c0e123
Marc GRIS <62659262+m-gris@users.noreply.github.com>
2025-04-02 07:13:30 +0800

    chore: dummy pages / routes

--- modules/app/src/main/scala/com/rockthejvm/reviewboard/components/Router.scala
+++ modules/app/src/main/scala/com/rockthejvm/reviewboard/components/Router.scala
@@ -11,13 +11,38 @@ object Router {
     mainTag(
       routes(
         div(
+
           cls := "container-fluid",
-          pathEnd { // localhost:1234 or  localhost:1234/
+
+          /*
+             DIRECTIVES
+             ----------
+             In the context of Scala web routing (specifically with the `frontroute` library ),
+             directives are BUILDING BLOCKS THAT COMPOSE THE ROUTING LOGIC.
+             They are similar to pattern matchers
+             and help determine what code should be executed based on the URL path and other HTTP request properties.
+           * */
+
+          (pathEnd | path("companies"))  {
+            // localhost:1234 or  localhost:1234/ or localhost:1234/companies or localhost:1234/companies/
+            // all redirect to the main page
             div("main page")
           },
-          path("companies") { // localhost:1234/companies
-            div("companies page")
+
+          path("login") {
+            // localhost:1234/login
+            div("login page")
+          },
+
+          path("signup") {
+            // localhost:1234/signup
+            div("signup page")
+          },
+
+          noneMatched {
+            div("404 page not found - Are you lost ?")
           }
+
           )
         )
       )
